d6e757535b009859c0e41cdec1cf3279990e4a86
Yuuto Harada <mryuu.dev@gmail.com>
2022-07-07 21:58:25 +0900

    feat: Complete (Hasura GraphQL 104)

--- rm_flutter_app/lib/main.dart
+++ rm_flutter_app/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:ferry/ferry.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rm_flutter_app/src/screens/all_recipes.dart';
 import 'package:rm_flutter_app/src/screens/new_recipe.dart';
 import 'package:rm_graphql_client/rm_graphql_client.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
+
   GetIt.instance.registerSingleton<Client>(
-    initClient('http://10.207.171.45:8081/v1/graphql')
+    initClient(
+      '${dotenv.env['HASURA_GRAPHQL_ENDPOINT']}/v1/graphql', 
+      dotenv.env['HASURA_GRAPHQL_ADMIN_SECRET']!,
+    )
   );
   runApp(const MyApp());
 }
